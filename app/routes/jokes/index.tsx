e802a8d81aafab2e2e71b477311101762037a05e
Dana Ciocan <danaciocan@economist.com>
2022-03-16 13:49:26 +0000

    Display random joke

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,28 @@
+import type { LoaderFunction } from "remix";
+import { json, useLoaderData } from "remix";
+import type { Joke } from "@prisma/client";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke };
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  return json({
+    joke: randomJoke,
+  });
+};
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{data?.joke?.content}</p>
     </div>
   );
 }
