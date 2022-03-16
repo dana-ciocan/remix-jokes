7e3c545dcbcd6e9520a7f712306ca88ee042cf4c
Dana Ciocan <danaciocan@economist.com>
2022-03-16 13:42:18 +0000

    Print out the joke matching the ID

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,26 @@
+import { json, useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+import type { Joke } from "@prisma/client";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke | null };
+export const loader: LoaderFunction = async ({ params }) => {
+  const data: LoaderData = {
+    joke: await db.joke.findUnique({
+      where: { id: params.jokeId },
+    }),
+  };
+  return json(data);
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+  console.log(data);
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data?.joke?.content}</p>
     </div>
   );
 }
